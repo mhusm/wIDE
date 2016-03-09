1b061d60ca3cecc7e0bceafe4a74f676eca9c282
stutzf <stutzf@student.ethz.ch>
2016-03-09 13:33:34 +0100

    Initial upload

---
+++ src/Query.java
@@ -0,0 +1,13 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * Created by fabian on 09.03.16.
+ */
+public class Query extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
