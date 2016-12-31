e0e412e8be072a7b8b244c4b2170ae45f061bffb
stutzf <stutzf@student.ethz.ch>
2016-12-31 12:04:50 +0100

    remove unnecessary files

--- src/ch/ethz/inf/globis/wide/parsing/AbstractLanguageHandler.java
+++
@@ -1,7 +0,0 @@
-package ch.ethz.inf.globis.wide.parsing;
-
-/**
- * Created by fabian on 17.03.16.
- */
-public class AbstractLanguageHandler {
-}
--- src/ch/ethz/inf/globis/wide/parsing/html/WideHtmlHandler.java
+++
@@ -1,7 +0,0 @@
-package ch.ethz.inf.globis.wide.parsing.html;
-
-/**
- * Created by fabian on 17.03.16.
- */
-public class WideHtmlHandler {
-}
